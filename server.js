e5be247a638b58175a590299d8b1eb84b21c89f4
Andy Kitt <andypkitt@gmail.com>
2018-10-28 10:04:30 +0000

    Prepared for Deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const startupDebug = require('debug', 'app:startup');
+const path = require('path');
 const users = require('./routes/users');
 const profile = require('./routes/profile');
 const passport = require('passport');
@@ -36,6 +37,17 @@ require('./config/passport.js')(passport);
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
+
+// Server static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
