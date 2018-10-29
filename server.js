4c32a0d0785e40acdbeb016afc9e1be91789042e
Andy Kitt <andypkitt@gmail.com>
2018-10-29 11:55:21 +0000

    fixed deploy issue

--- server.js
+++ server.js
@@ -51,4 +51,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`Server listening on port ${port}...`));
+app.listen(port, () => console.log(`Server listening on port ${port}...`));
